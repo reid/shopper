06dbb64f520b85f3416cf9e364effb3d9d15f9ee
Reid Burke <me@reidburke.com>
2009-05-18 15:29:13 -0700

    Wrap setQuery in script tag

--- canvas.php
+++ canvas.php
@@ -3,7 +3,9 @@
 echo file_get_contents('index.html');
 
 if (array_key_exists('q', $_REQUEST)) {
-    echo <<<EOJS
-IRShopper.setQuery("{$_REQUEST['q']}");
-EOJS;
+    echo <<<EOHTML
+<script>
+    IRShopper.setQuery("{$_REQUEST['q']}");
+</script>
+EOHTML;
 }
